24d42a79a4c0ae22077fa24dd4314640f78b4430
WingsDevelopment <bans.cowboy@gmail.com>
2024-09-16 00:28:43 +0200

    added accs

--- config.js
+++ config.js
@@ -3,11 +3,24 @@ module.exports = {
   TELEGRAM_CHAT_IDS: ["6416507389", "6233329129"],
   SIZE_CHANGE_THRESHOLD: 5,
   MONITOR_URLS: [
-    "https://app.gmx.io/#/accounts/0x5Cf259c7e51C497c6aFD3807266E75140aCa381A?network=arbitrum&v=2",
-    "https://app.gmx.io/#/accounts/0xDB611d682cb1ad72fcBACd944a8a6e2606a6d158?network=arbitrum&v=2",
-    "https://app.gmx.io/#/accounts/0x686Ed15ce2d95121313E00Cd8777aFc228Ece46f?network=arbitrum&v=2",
-    "https://app.gmx.io/#/accounts/0x5Cf259c7e51C497c6aFD3807266E75140aCa381A?network=arbitrum&v=2",
+    // avalanche
+    // top 1
     "https://app.gmx.io/#/accounts/0x4Cd80aa0CE4881Eb8679EdA1f6fbe3d89AEc0F7F?network=avalanche&v=2",
+    // top 2
+    "https://app.gmx.io/#/accounts/0xd01051C643f839EBB8989FF29F090A674f3E39E4?network=avalanche&v=2",
+    // top 3
+    "https://app.gmx.io/#/accounts/0xf2d83482f8C835423C2901b1c67081f726E39B25?network=avalanche&v=2",
+    // top 4
+    "https://app.gmx.io/#/accounts/0xEAf9A74BA2928A903366bD42339f1bbf1cbdaa59?network=avalanche&v=2",
+    // top 5
+    "https://app.gmx.io/#/accounts/0x2ac1fA38a82738c3875B3ac385Fbade067e3D123?network=avalanche&v=2",
+    // top 6
+    "https://app.gmx.io/#/accounts/0xBc43e519f68c8b8d0605D45A2f298b3D86ff5c5F?network=avalanche&v=2",
+    // arbitrum
+    "https://app.gmx.io/#/accounts/0x2c2f76429EAB8E2730BE4085dcdd9370B8D1a96c?network=arbitrum&v=2",
+    "https://app.gmx.io/#/accounts/0x45246498353a9E3CA808B67567E238865350FC10?network=arbitrum&v=2",
+    "https://app.gmx.io/#/accounts/0x9fC3B6191927B044EF709addd163b15c933EE205?network=arbitrum&v=2",
+    "https://app.gmx.io/#/accounts/0x5CE6f3798B9ca0797E1027E9b86E7dF0Ba61E593?network=arbitrum&v=2",
   ],
   TIME_OUT_MS: 5 * 60 * 1000,
   OTHER_TIME_OUTS: 35 * 1000,
