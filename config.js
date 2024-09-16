e189939044ed66670f981f342e9b98d37ac43863
WingsDevelopment <bans.cowboy@gmail.com>
2024-09-16 12:16:12 +0200

    changed config

--- config.js
+++ config.js
@@ -3,54 +3,18 @@ module.exports = {
   TELEGRAM_CHAT_IDS: ["6416507389"], //"6233329129"
   SIZE_CHANGE_THRESHOLD: 5,
   MONITOR_URLS: [
-    // {
-    //   OwnerName: "/",
-    //   Description: "Top 1 Avalanche account (seems experienced)",
-    //   Url: "https://app.gmx.io/#/accounts/0x4Cd80aa0CE4881Eb8679EdA1f6fbe3d89AEc0F7F?network=avalanche&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Top 2 Avalanche account",
-    //   Url: "https://app.gmx.io/#/accounts/0xd01051C643f839EBB8989FF29F090A674f3E39E4?network=avalanche&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Top 3 Avalanche account",
-    //   Url: "https://app.gmx.io/#/accounts/0xf2d83482f8C835423C2901b1c67081f726E39B25?network=avalanche&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Was in - for long time, now finally breaking out",
-    //   Url: "https://app.gmx.io/#/accounts/0xBc43e519f68c8b8d0605D45A2f298b3D86ff5c5F?network=avalanche&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Taking loses and wins, but going in +",
-    //   Url: "https://app.gmx.io/#/accounts/0x2c2f76429EAB8E2730BE4085dcdd9370B8D1a96c?network=arbitrum&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Was in -0.5M PNL, now +0.7M PNL",
-    //   Url: "https://app.gmx.io/#/accounts/0x45246498353a9E3CA808B67567E238865350FC10?network=arbitrum&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Took some loses, but generaly in +",
-    //   Url: "https://app.gmx.io/#/accounts/0x9fC3B6191927B044EF709addd163b15c933EE205?network=arbitrum&v=2",
-    //   OurRating: "/",
-    // },
-    // {
-    //   OwnerName: "/",
-    //   Description: "Very high win%, no loses",
-    //   Url: "https://app.gmx.io/#/accounts/0x5CE6f3798B9ca0797E1027E9b86E7dF0Ba61E593?network=arbitrum&v=2",
-    //   OurRating: "/",
-    // },
+    {
+      OwnerName: "/",
+      Description: "Top 1 Avalanche account (seems experienced)",
+      Url: "https://app.gmx.io/#/accounts/0x4Cd80aa0CE4881Eb8679EdA1f6fbe3d89AEc0F7F?network=avalanche&v=2",
+      OurRating: "/",
+    },
+    {
+      OwnerName: "/",
+      Description: "Very high win%, no loses",
+      Url: "https://app.gmx.io/#/accounts/0x5CE6f3798B9ca0797E1027E9b86E7dF0Ba61E593?network=arbitrum&v=2",
+      OurRating: "/",
+    },
     {
       OwnerName: "/",
       Description: "Was -3.5M PNL, now +2.5M PNL (Flame)",
@@ -59,8 +23,8 @@ module.exports = {
     },
     {
       OwnerName: "/",
-      Description: "Was -3.5M PNL, now +2.5M PNL (Flame)",
-      Url: "https://app.gmx.io/#/accounts/0x4Cd80aa0CE4881Eb8679EdA1f6fbe3d89AEc0F7F?network=arbitrum&v=2",
+      Description: "Waiting to long btc",
+      Url: "https://app.gmx.io/#/accounts/0xB6860393Ade5CD3766E47e0B031A0F4C33FD48a4?network=arbitrum&v=2",
       OurRating: "/",
     },
     {
