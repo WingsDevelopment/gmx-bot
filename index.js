05cfa52237e062af8937b0e1c043377b7c4785c1
WingsDevelopment <bans.cowboy@gmail.com>
2024-09-17 22:29:55 +0200

    deleted chrone job

--- index.js
+++ index.js
@@ -185,17 +185,6 @@ function init() {
       );
     }
   });
-
-  // Optional: Periodically log memory usage for monitoring
-  cron.schedule("*/2 * * * *", () => {
-    const memoryUsage = process.memoryUsage();
-    console.log("Memory Usage:", {
-      rss: `${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`,
-      heapTotal: `${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB`,
-      heapUsed: `${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`,
-      external: `${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB`,
-    });
-  });
 }
 
 // Start the bot
